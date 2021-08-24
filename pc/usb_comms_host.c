29fd6e49f934ce7fd3134dfc499547383b67d1da
Chainmanner <pompilica2000@yahoo.com>
2021-08-24 17:55:39 -0400

    Added GPLv3 license notice headers.

--- pc/usb_comms_host.c
+++ pc/usb_comms_host.c
@@ -1,5 +1,21 @@
 // COMP4905 - Honours Project, Carleton University
 // Gabriel Valachi (101068875)
+/*
+	Copyright (C) 2021	Gabriel Valachi
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 // This file contains the code needed for USB communications.
 // Note that the USB interface is not found or claimed here; this needs to be done before any of the exported functions can be used.
--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,5 +1,21 @@
 # COMP4905 - Honours Project, Carleton University
 # Gabriel Valachi (101068875)
+#
+#	Copyright (C) 2021	Gabriel Valachi
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 # Contains some definitions needed to build the recovery ramdisk.
 # NOTE: Turns out this file isn't necessary, but I'll keep it in anyway.
